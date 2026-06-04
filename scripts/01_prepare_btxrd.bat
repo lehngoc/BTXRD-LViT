5145ed1138a4f01e2b90cefcb58b587107cd7265
lehngoc <lhngoc23@clc.fitus.edu.vn>
2026-06-05 01:20:18 +0700

    Update BTXRD preprocessing pipeline script

--- scripts/01_prepare_btxrd.bat
+++ scripts/01_prepare_btxrd.bat
@@ -4,3 +4,4 @@ python src\preprocessing\visualize_masks.py
 python src\preprocessing\preprocess_btxrd_data.py --clean-output
 python src\preprocessing\validate_preprocessed_data.py
 python src\preprocessing\visualize_preprocessed_data.py
+python src\preprocessing\make_splits.py
